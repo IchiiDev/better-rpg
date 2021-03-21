9da20b31edab9ee06146f81d9a7df68e1b9e8500
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-03-21 21:53:16 +0100

    Implement item interaction

--- src/main/java/xyz/ichiidev/betterrpg/events/ClickItem.java
+++ src/main/java/xyz/ichiidev/betterrpg/events/ClickItem.java
@@ -1,14 +1,23 @@
 package xyz.ichiidev.betterrpg.events;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class ClickItem implements Listener {
 
     @EventHandler
-    public void onItemClick(PlayerItemConsumeEvent event) {
-
+    public void onItemClick(PlayerInteractEvent event) {
+        ItemStack item = event.getItem();
+        ItemMeta meta = item.getItemMeta();
+        event.getPlayer().sendMessage(item.getType().name() + " " + meta.getDisplayName());
+        System.out.println(item.getType() == Material.WRITTEN_BOOK && meta.getDisplayName().equals("§3The Quest Grimory"));
+        if (item.getType() == Material.WRITTEN_BOOK && meta.getDisplayName().equals("§3The Quest Grimory")) {
+            event.setCancelled(true);
+        }
     }
 
 }
